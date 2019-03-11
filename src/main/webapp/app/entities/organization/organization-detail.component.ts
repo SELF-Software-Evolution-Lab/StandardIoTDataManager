a2529e821a70c10a9ff6dbc61212f11f4de92b90
Alfonso Ardila <ad.ardila@uniandes.edu.co>
2019-03-11 13:13:23 -0500

    feat: Se agrega la visualización de lo suaurios en el detalle de la organización.
    Queda pendien realizar filtro para que solo muestre  los usuarios de la organización que se esta viendo el detalle.

--- src/main/webapp/app/entities/organization/organization-detail.component.ts
+++ src/main/webapp/app/entities/organization/organization-detail.component.ts
@@ -1,7 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpResponse } from '@angular/common/http';
 
-import {IOrganization} from 'app/shared/model/organization.model';
+import { IOrganization } from 'app/shared/model/organization.model';
+import { UserService, User } from 'app/core';
+import { ITEMS_PER_PAGE } from 'app/shared';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-organization-detail',
@@ -9,16 +13,37 @@ import {IOrganization} from 'app/shared/model/organization.model';
 })
 export class OrganizationDetailComponent implements OnInit {
     organization: IOrganization;
+    users: User[];
 
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    constructor(protected activatedRoute: ActivatedRoute, private userService: UserService, private alertService: JhiAlertService) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ organization }) => {
             this.organization = organization;
+            this.loadAllUsers();
         });
     }
 
     previousState() {
         window.history.back();
+    }
+
+    loadAllUsers() {
+        console.log('probando');
+        this.userService
+            .query()
+            .subscribe(
+                (res: HttpResponse<User[]>) => this.onSuccessUsers(res.body, res.headers),
+                (res: HttpResponse<any>) => this.onErrorUsers(res.body)
+            );
+    }
+
+    private onSuccessUsers(data, headers) {
+        this.users = data;
+    }
+
+    private onErrorUsers(error) {
+        console.log('Error usuarios');
+        this.alertService.error(error.error, error.message, null);
     }
 }
