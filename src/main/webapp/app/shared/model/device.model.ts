2d4d46cd124a9951610549f2e8d88647ae18ff92
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-03-25 15:31:29 -0500

    [FM] adjust experiment and sampling with detail relationships

--- src/main/webapp/app/shared/model/device.model.ts
+++ src/main/webapp/app/shared/model/device.model.ts
@@ -1,12 +1,9 @@
-import { ISensor } from 'app/shared/model/sensor.model';
-
 export interface IDevice {
     internalId?: string;
     name?: string;
     description?: string;
-    sensors?: ISensor[];
 }
 
 export class Device implements IDevice {
-    constructor(public internalId?: string, public name?: string, public description?: string, public sensors?: ISensor[]) {}
+    constructor(public internalId?: string, public name?: string, public description?: string) {}
 }
