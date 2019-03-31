e462fe103c66f60c24bcf754a5ced3f157efd89b
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-03-30 21:40:25 -0500

    [FM] enable swagger

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,30 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'organization',
+                loadChildren: './organization/organization.module#XrepoOrganizationModule'
+            },
+            {
+                path: 'target-system',
+                loadChildren: './target-system/target-system.module#XrepoTargetSystemModule'
+            },
+            {
+                path: 'experiment',
+                loadChildren: './experiment/experiment.module#XrepoExperimentModule'
+            },
+            {
+                path: 'sampling',
+                loadChildren: './sampling/sampling.module#XrepoSamplingModule'
+            },
+            {
+                path: 'sample',
+                loadChildren: './sample/sample.module#XrepoSampleModule'
+            },
+            {
+                path: 'tag',
+                loadChildren: './tag/tag.module#XrepoTagModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
