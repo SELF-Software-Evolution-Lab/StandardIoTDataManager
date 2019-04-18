a2ff420ca07d89988fb256c549e820f250d709d2
alfyesUA <ad.ardila@uniandes.edu.co>
2019-04-18 15:38:40 -0500

    feat: Se crea consulta para la obteción de archivos por el estado.

--- src/main/java/co/edu/uniandes/xrepo/repository/SamplesFilesRepository.java
+++ src/main/java/co/edu/uniandes/xrepo/repository/SamplesFilesRepository.java
@@ -2,6 +2,9 @@ package co.edu.uniandes.xrepo.repository;
 
 import co.edu.uniandes.xrepo.domain.SamplesFiles;
 import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +15,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface SamplesFilesRepository extends MongoRepository<SamplesFiles, String> {
+
+    List<SamplesFiles> findByState(int state);
 
 }
