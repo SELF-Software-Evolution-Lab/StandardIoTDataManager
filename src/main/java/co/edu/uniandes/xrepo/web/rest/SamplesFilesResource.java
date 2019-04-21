75c122ad8e5215afe6999ca1fe92d456dbab5c7d
alfyesUA <ad.ardila@uniandes.edu.co>
2019-04-20 21:15:21 -0500

    feat: Se activa que al subir un archivo se ejecute la tarea en batch para el procesar el archivo.

--- src/main/java/co/edu/uniandes/xrepo/web/rest/SamplesFilesResource.java
+++ src/main/java/co/edu/uniandes/xrepo/web/rest/SamplesFilesResource.java
@@ -21,6 +21,15 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * REST controller for managing SamplesFiles.
@@ -34,6 +43,12 @@ public class SamplesFilesResource {
     private static final String ENTITY_NAME = "samplesFiles";
 
     private final SamplesFilesService samplesFilesService;
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job processJob;
 
     public SamplesFilesResource(SamplesFilesService samplesFilesService) {
         this.samplesFilesService = samplesFilesService;
@@ -91,11 +106,19 @@ public class SamplesFilesResource {
 
             result = samplesFilesService.save(samplesFilesDTO2);
 
-           
-
         } catch (IllegalStateException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).addString("filePath", "C:\\AJAR\\SamplesFiles\\" + fileTemp.getName())
+                .toJobParameters();
+        try {
+            jobLauncher.run(processJob, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
+                | JobParametersInvalidException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
