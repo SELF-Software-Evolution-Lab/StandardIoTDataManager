b343af934dc11aeb775e6d09902130335f0281b0
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-04-26 22:46:34 -0500

    [FM] Agregar ExecutorPool para correr tareas pendientes por ser procesadas

--- src/main/java/co/edu/uniandes/xrepo/service/task/BackgroundTaskExecutor.java
+++ src/main/java/co/edu/uniandes/xrepo/service/task/BackgroundTaskExecutor.java
@@ -7,7 +7,6 @@ import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
