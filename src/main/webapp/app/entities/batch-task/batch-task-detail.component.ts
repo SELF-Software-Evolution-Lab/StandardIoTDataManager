1ccd14718080bd38b3a42dd7f7a22bb38e9fe3eb
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-05-05 17:58:14 -0500

    [FM] Ajustar detalle de tareas de busqueda de muestras

--- src/main/webapp/app/entities/batch-task/batch-task-detail.component.ts
+++ src/main/webapp/app/entities/batch-task/batch-task-detail.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { IBatchTask } from 'app/shared/model/batch-task.model';
+import { IBatchTask, TaskState, TaskType } from 'app/shared/model/batch-task.model';
+import { BatchTaskService } from 'app/entities/batch-task/batch-task.service';
 
 @Component({
     selector: 'jhi-batch-task-detail',
     templateUrl: './batch-task-detail.component.html'
 })
 export class BatchTaskDetailComponent implements OnInit {
+    taskTypeFileLoad = TaskType.FILELOAD;
+    taskTypeReport = TaskType.REPORT;
+
     batchTask: IBatchTask;
 
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    constructor(protected activatedRoute: ActivatedRoute, protected batchTaskService: BatchTaskService) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ batchTask }) => {
@@ -20,5 +24,17 @@ export class BatchTaskDetailComponent implements OnInit {
 
     previousState() {
         window.history.back();
+    }
+
+    locateReport(id: string): string {
+        return this.batchTaskService.locateReport(id);
+    }
+
+    isCompleted(state: TaskState) {
+        return state === TaskState.COMPLETED;
+    }
+
+    isError(state: TaskState) {
+        return state === TaskState.ERROR;
     }
 }
