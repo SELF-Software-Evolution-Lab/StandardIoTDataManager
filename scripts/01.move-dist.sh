fb24f9440211561f38fcaecbb9e58376d2a6c6db
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-05-26 22:49:04 -0500

    [FM] Agregar scripts para facilitar la ejecucion

--- scripts/01.move-dist.sh
+++ scripts/01.move-dist.sh
@@ -2,7 +2,8 @@
 rm -rf /home/profesor/xrepo-bin
 mkdir /home/profesor/xrepo-bin
 
-yes | cp -a /home/profesor/development/StandardIoTDataManager/scripts/run-web.sh /home/profesor/xrepo-bin/run-web.sh
-yes | cp -a /home/profesor/development/StandardIoTDataManager/scripts/run-worker.sh /home/profesor/xrepo-bin/run-worker.sh
-yes | cp /home/profesor/development/StandardIoTDataManager/build/libs/xrepo-0.0.1-SNAPSHOT.war /mnt/nfs/var/nfs/app/
+cp -a /home/profesor/development/StandardIoTDataManager/scripts/run-web.sh /home/profesor/xrepo-bin/run-web.sh
+cp -a /home/profesor/development/StandardIoTDataManager/scripts/run-worker.sh /home/profesor/xrepo-bin/run-worker.sh
+cp /home/profesor/development/StandardIoTDataManager/build/libs/xrepo-0.0.1-SNAPSHOT.war /home/profesor/xrepo-bin/
+
 
