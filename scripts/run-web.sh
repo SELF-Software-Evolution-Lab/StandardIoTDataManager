f9e6a0ee402482e6b1504ee624863b09a74baf7c
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-05-26 22:15:50 -0500

    [FM] Agregar scripts para facilitar la ejecucion

--- scripts/run-web.sh
+++ scripts/run-web.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
+yes | cp -a /mnt/nfs/var/nfs/app/. /home/profesor/development/StandardIoTDataManager/
 cd /home/profesor/development/StandardIoTDataManager
 nohup java -jar /home/profesor/development/StandardIoTDataManager/xrepo-0.0.1-SNAPSHOT.war --server.port=80 >/dev/null 2>&1 &
+echo "go to cd /home/profesor/development/StandardIoTDataManager/logs to check app status"
