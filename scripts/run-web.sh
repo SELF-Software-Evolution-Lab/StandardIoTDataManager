bb4c4769f30837be58bf9a95cf74d99c4062df2b
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-05-26 22:10:34 -0500

    [FM] Agregar scripts para facilitar la ejecucion

--- scripts/run-web.sh
+++ scripts/run-web.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-
+cd /home/profesor/development/StandardIoTDataManager
+nohup java -jar /home/profesor/development/StandardIoTDataManager/xrepo-0.0.1-SNAPSHOT.war --server.port=80 >/dev/null 2>&1 &
