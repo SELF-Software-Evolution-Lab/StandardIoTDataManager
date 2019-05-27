0a014c68e553063dd81a4ba2e14fe1b4111994b6
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-05-26 22:22:29 -0500

    [FM] Agregar scripts para facilitar la ejecucion

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
-
-yes | cp -r /home/profesor/development/StandardIoTDataManager/scripts /mnt/nfs/var/nfs/app/scripts
+rm -rf /mnt/nfs/var/nfs/app
+mkdir /mnt/nfs/var/nfs/app
+mkdir /mnt/nfs/var/nfs/app/scripts
+yes | cp -a /home/profesor/development/StandardIoTDataManager/scripts/. /mnt/nfs/var/nfs/app/scripts/
 yes | cp /home/profesor/development/StandardIoTDataManager/build/libs/xrepo-0.0.1-SNAPSHOT.war /mnt/nfs/var/nfs/app/
 
