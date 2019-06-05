9959ccd1a0215ebf2a05f7da0cc36f893ec3fdf5
Felipe Martinez <f.martinez2@uniandes.edu.co>
2019-06-04 21:28:55 -0500

    [FM] build script

--- scripts/00.build.sh
+++ scripts/00.build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd /home/profesor/development/StandardIoTDataManager
 
-chmod 777 /home/profesor/development/StandardIoTDataManager/.gradlew
+chmod 777 /home/profesor/development/StandardIoTDataManager/gradlew
 
 ./gradlew clean -Pprod bootWar
